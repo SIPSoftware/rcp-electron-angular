7a74902d3cd82c4718c5c0dbe965b56e6e868595
Aleksander Kupiec <olokup@gmail.com>
2023-03-24 17:19:57 +0100

    wprowadzenie styli material

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,9 +6,16 @@ import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 import { CoreModule } from '../core/core.module';
+import { MaterialModule } from '../shared/material.module';
 
 @NgModule({
     declarations: [HomeComponent],
-    imports: [CommonModule, SharedModule, HomeRoutingModule, CoreModule],
+    imports: [
+        CommonModule,
+        SharedModule,
+        HomeRoutingModule,
+        CoreModule,
+        MaterialModule,
+    ],
 })
 export class HomeModule {}
